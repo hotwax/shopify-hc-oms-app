0db78ce1c3c6991e94962b400c28a92b3c737bbc
Azkya Khan <azkya.khan@hotwax.co>
2021-09-20 10:15:27 +0530

    Implemented: Internationalization on hardcoded static codes (#8zcc02-internationalization)

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,12 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import i18n from './i18n'
+
+
 const app = createApp(App)
   .use(IonicVue)
+  .use(i18n)
   .use(router);
   
 router.isReady().then(() => {
