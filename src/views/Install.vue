3abbe4a8a03452fbf11aefef3e9dd84f9c90b5ce
Yash Maheshwari <yash.maheshwari@hotwax.co>
2021-09-30 16:12:25 +0530

    Improved: code to get parameters from route, also updated the redirectUri value(#cx50mq)

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -15,7 +15,7 @@
           </ion-item>
         </ion-list>
         <div class="ion-padding">
-          <ion-button type="submit" expand="block">
+          <ion-button type="submit" expand="block" @click="install">
             Install
           </ion-button>
         </div>
@@ -54,30 +54,38 @@ export default defineComponent({
   },
   data() {
     return {
-      apiKey: "",
-      shopOrigin: "hc-sandbox.myshopify.com",
+      apiKey: process.env.VUE_APP_SHOPIFY_API_KEY,
+      shopOrigin: 'hc-sandbox.myshopify.com',
+      session: this.$route.query['session'],
+      hmac: this.$route.query['hmac'],
+      shop: this.$route.query['shopOrigin'],
+      host: this.$route.query['host'],
+      locale: this.$route.query['locale'] || process.env.VUE_APP_I18N_LOCALE || process.env.VUE_APP_I18N_FALLBACK_LOCALE,
+      timestamp: this.$route.query['timestamp'],
+      code: this.$route.query['code']
     };
   },
+  mounted () {
+    if (this.session) {
+      emitter.emit("presentLoader");
+      this.$router.push("/");
+    } else if (this.code) {
+      const appURL = `https://${this.shop}/admin/apps/${this.apiKey}`;
+      window.location.assign(appURL);
+    } else if (this.shop || this.host) {
+      this.authorise(this.shop, this.host, this.apiKey);
+    } else {
+      emitter.emit("dismissLoader");
+    }
+  },
   methods: {
-    install(shopOrigin: any, shop: any, host: any, session: any, code: any) {
+    install(shopOrigin: any) {
       this.authorise(shopOrigin, undefined, this.apiKey);
-      {
-        if (session) {
-          emitter.emit("presentLoader");
-          this.$router.push("/");
-        } else if (code) {
-          const appURL = `https://${shop}/admin/apps/${this.apiKey}`;
-          window.location.assign(appURL);
-        } else if (shop || host) {
-          this.authorise(shop, host, this.apiKey);
-        } else {
-          emitter.emit("dismissLoader");
-        }
-      }
     },
     authorise(shop: any, host: any, apiKey: any) {
-      const redirectUri = process.env.SHOPIFY_REDIRECT_URI;
+      const redirectUri = process.env.VUE_APP_SHOPIFY_REDIRECT_URI;
       const permissionUrl = `https://${shop}/admin/oauth/authorize?client_id=${apiKey}&scope=read_products,read_content&redirect_uri=${redirectUri}`;
+
       if (window.top == window.self) {
         window.location.assign(permissionUrl);
       } else {
